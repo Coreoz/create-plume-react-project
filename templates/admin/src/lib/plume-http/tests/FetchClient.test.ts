cc97952ab7d20a249520d42e06b9db962f8c7c53
Aurélien Manteaux <amanteaux@coreoz.com>
2022-01-20 10:49:46 +0100

    Add unit tests

--- templates/admin/src/lib/plume-http/tests/FetchClient.test.ts
+++ templates/admin/src/lib/plume-http/tests/FetchClient.test.ts
@@ -1,5 +1,9 @@
 import ApiHttpClient from '../../../api/ApiHttpClient';
 import HttpMethod from '../../simple-http-request-builder/HttpMethod';
+import { genericError, networkError, timeoutError } from '../client/HttpResponse';
+import fetchClient from '../client/FetchClient';
+import HttpRequest from '../../simple-http-request-builder/HttpRequest';
+import { unwrapHttpPromise } from '../promise/HttpPromise';
 
 let mockedFetchResponseBody: string | undefined;
 const mockedFetchResponseStatus: ResponseInit = {
@@ -13,7 +17,7 @@ const setMockedBody = (responseObject: object) => {
   mockedFetchResponseBody = JSON.stringify(responseObject);
 };
 
-const mockedFetch = (input: RequestInfo, init?: RequestInit): Promise<Response> => {
+const mockedFetch = (): Promise<Response> => {
   if (mockedPromiseError) {
     return Promise.reject(mockedPromiseError);
   }
@@ -26,6 +30,7 @@ describe('Tests on Login', () => {
 
   beforeEach(() => {
     mockedPromiseError = undefined;
+    mockedFetchResponseBody = undefined;
   });
 
   afterAll(() => {
@@ -41,5 +46,37 @@ describe('Tests on Login', () => {
     expect(response.status).toBe(200);
     const body = await response.json();
     expect(body).toEqual({ test: 1 });
+  });
+
+  test('Check network error rejects promise with networkError', async () => {
+    mockedPromiseError = new Error();
+    const response = new ApiHttpClient()
+      .rawRequest(HttpMethod.GET, '/users')
+      .execute()
+      .toPromise();
+    await expect(response).rejects.toEqual(networkError);
+  });
+
+  test('Check timeout error rejects promise with timeoutError', async () => {
+    mockedPromiseError = new Error();
+    mockedPromiseError.name = 'AbortError';
+    const response = new ApiHttpClient()
+      .rawRequest(HttpMethod.GET, '/users')
+      .execute()
+      .toPromise();
+    await expect(response).rejects.toEqual(timeoutError);
+  });
+
+  test('Check handler execution error rejects promise with genericError', async () => {
+    const httpClientWithErrorHandler = (httpRequest: HttpRequest<unknown>) => unwrapHttpPromise(fetchClient(
+      httpRequest, () => { throw new Error(); },
+    ));
+    const response = new HttpRequest(
+      httpClientWithErrorHandler,
+      'http://localhost',
+      HttpMethod.GET,
+      '/users',
+    ).execute();
+    await expect(response).rejects.toEqual(genericError);
   });
 });
