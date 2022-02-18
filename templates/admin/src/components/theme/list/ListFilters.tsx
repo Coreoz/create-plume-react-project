ca58f4e56b48fe91ca3a12ba15a82af783763b12
Lucas Amiaud <lamiaud@coreoz.com>
2022-02-18 11:55:41 +0100

    remove useless class and add ts doc

--- templates/admin/src/components/theme/list/ListFilters.tsx
+++ templates/admin/src/components/theme/list/ListFilters.tsx
@@ -8,6 +8,17 @@ import {
   ObjectFilterProps
 } from '../../../lib/plume-admin-theme/list/ListProps';
 
+/**
+ * ListObjectFilters generates filters for a give object type
+ * @param filterMenuKey the key in translation file to be used
+ * @param onFilterValueClicked function executed when a checkbox is clicked
+ * @param filters The filters for the given type.
+ *                Each filter must contain a key extractor of the given object
+ *                Each filter must contain a key filter for identification
+ * @param rawList the whole given type list to be filtered
+ * @param selectedValues the map of the current selected values by key filter
+ * @constructor
+ */
 export function ListObjectFilters<T>(
   {
     filterMenuKey,
@@ -33,6 +44,16 @@ export function ListObjectFilters<T>(
   )
 }
 
+/**
+ * ListFilters is a generic component for filtering in a list
+ * @param filterMenuKey the key in translation file to be used
+ * @param filters The filters to be displayed.
+ *                Each filter must contain possible values
+ *                Each filter must contain a key filter for identification
+ * @param onFilterValueClicked function executed when a checkbox is clicked
+ * @param selectedValues the map of the current selected values by key filter
+ * @constructor
+ */
 function ListFilters(
   { filterMenuKey, filters, onFilterValueClicked, selectedValues }: ListRawFiltersProps
 ) {
@@ -52,21 +73,22 @@ function ListFilters(
               {
                 Array.from(new Set<string>([...filterPossibility.possibleValues]))
                   .map((value: string) => (
-                    <FormControlLabel
-                      key={value}
-                      label={value}
-                      control={(
-                        <Checkbox
-                          value={value}
-                          size={CHECK_BOX_SIZE}
-                          checked={selectedValues.get(filterPossibility.filterKey)?.includes(value) || false}
-                          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            onFilterValueClicked(filterPossibility.filterKey, value, e.target.checked);
-                          }}
-                        />
-                      )}
-                    />
-                  ))
+                      <FormControlLabel
+                        key={value}
+                        label={value}
+                        control={(
+                          <Checkbox
+                            value={value}
+                            size={CHECK_BOX_SIZE}
+                            checked={selectedValues.get(filterPossibility.filterKey)?.includes(value) || false}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                              onFilterValueClicked(filterPossibility.filterKey, value, e.target.checked);
+                            }}
+                          />
+                        )}
+                      />
+                    )
+                  )
               }
             </div>
           ))
