0ae15f022b773febf94ece6dc3ce4af2f13b5c23
Lucas Amiaud <lamiaud@coreoz.com>
2022-03-21 10:48:01 +0100

    rollback disconnect function

--- templates/admin/src/components/layout/Header.tsx
+++ templates/admin/src/components/layout/Header.tsx
@@ -42,7 +42,7 @@ export default function Header({ currentLocale, currentUser }: HeaderProps) {
               <DropdownMenu label={getInitialLettersOfUser() || ''} id="user-menu">
                 <div id="user-name">{currentUser.fullName}</div>
                 <MenuItem
-                  onClick={sessionService.disconnect}
+                  onClick={() => sessionService.disconnect()}
                 >
                   {messages['action.disconnect']}
                 </MenuItem>
