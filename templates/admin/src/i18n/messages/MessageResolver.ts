94ce041568424030c50ddeb61b454154f81c870f
Lucas Amiaud <lamiaud@coreoz.com>
2022-06-10 09:38:37 +0200

    AMA PR feedback

--- templates/admin/src/i18n/messages/MessageResolver.ts
+++ templates/admin/src/i18n/messages/MessageResolver.ts
@@ -19,8 +19,11 @@ export default class MessageResolver implements PlumeMessageResolver {
    * This function reads a message key like 'tree.node.message_key'
    * Then splits it into a tab ['tree', 'node', 'message']
    * Then recursively going down the Translation tree until finding a word
+   *
+   * Enables to find the message value in a object (e.g `{a: {b: {c: value}}}` with a string key (e.g `a.b.c`)
+   *
    * @param messageKey: the message key like 'tree.node.message_key'
-   * @param messageArgs: the potentials message arguments of the message
+   * @param messageArgs: the potentials arguments of the message
    */
   private messageResolver = (messageKey: string, ...messageArgs: string[]): string => {
     const translation = messageKey.split('.')
