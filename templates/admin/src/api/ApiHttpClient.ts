803d9d9106e20b7d3c49406906e1b4ea2df49a1b
Aurélien Manteaux <amanteaux@coreoz.com>
2022-06-24 18:53:58 +0200

    Upgrade simple-http-rest-client

--- templates/admin/src/api/ApiHttpClient.ts
+++ templates/admin/src/api/ApiHttpClient.ts
@@ -1,6 +1,6 @@
-import { HttpRequest, HttpMethod } from 'simple-http-request-builder';
+import { HttpMethod, HttpRequest } from 'simple-http-request-builder';
 import {
-  defaultJsonFetchClient, fetchClient, HttpPromise, unwrapHttpPromise,
+  createHttpFetchRequest, defaultJsonFetchClient, fetchClient, HttpPromise,
 } from 'simple-http-rest-client';
 
 const baseUrl = '/api';
@@ -8,27 +8,11 @@ const baseUrl = '/api';
 export default class ApiHttpClient {
   // eslint-disable-next-line class-methods-use-this
   rawRequest(method: HttpMethod, path: string): HttpRequest<HttpPromise<Response>> {
-    return new HttpRequest<HttpPromise<Response>>(
-      (httpRequest) => new HttpPromise<Response>(
-        unwrapHttpPromise(fetchClient(httpRequest)),
-        httpRequest,
-      ),
-      baseUrl,
-      method,
-      path,
-    );
+    return createHttpFetchRequest(baseUrl, method, path, fetchClient);
   }
 
   // eslint-disable-next-line class-methods-use-this
   restRequest<T>(method: HttpMethod, path: string): HttpRequest<HttpPromise<T>> {
-    return new HttpRequest<HttpPromise<T>>(
-      (httpRequest) => new HttpPromise<T>(
-        unwrapHttpPromise(defaultJsonFetchClient(httpRequest)),
-        httpRequest,
-      ),
-      baseUrl,
-      method,
-      path,
-    );
+    return createHttpFetchRequest(baseUrl, method, path, defaultJsonFetchClient);
   }
 }
