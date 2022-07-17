28a77cc0dd0e0b1ee48fc97c77c0424d05d8b4ac
wlatif <wlatif-ext@coreoz.com>
2022-07-17 14:52:36 +0200

    ajout d'un alias pour le fichier _variables.scss

--- templates/front/vite.config.ts
+++ templates/front/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [reactRefresh()],
+    resolve: {
+        alias: {
+            '@scssVariables' : path.resolve(__dirname,'./assets/scss/_variables.scss')
+        },
+    },
     build: {
         outDir: 'build'
     },
