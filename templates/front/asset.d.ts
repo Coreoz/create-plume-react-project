5832107999a8c654a48e52ad44ab784844d9504c
wlatif <wlatif-ext@coreoz.com>
2022-07-17 14:05:32 +0200

    Ajout du transformer non-ts-module-import-transformer.ts permettant de mettre à jour les imports scss dans les fichier js lors de la compilation ts pour faire reference au modules dans src.
    (utilisable pour d'autre import de fichier non ts)

--- templates/front/asset.d.ts
+++ templates/front/asset.d.ts
@@ -2,12 +2,11 @@
  * this file is use to make import available on different file type than .ts or .tsx .js .css etc.
  * You can declare here any type of file that you want to import
  */
-
-declare module '*.png'
-declare module '*.jpg'
-declare module '*.gif'
-
 /**
  * ajout des modules vite
  */
 /// <reference types="vite/client" />
+
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.gif'
