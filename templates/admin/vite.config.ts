786e46b3c3c29aee6e59b1c0a193ae6826cf3d0e
Aurélien Manteaux <amanteaux@coreoz.com>
2022-12-27 18:58:34 +0100

    Use eslint also for config files

--- templates/admin/vite.config.ts
+++ templates/admin/vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [reactRefresh()],
-    // uncomment the line with the base attribute to use the context path /admin/
-    // base: '/admin/',
-    build: {
-        outDir: 'build'
+  plugins: [reactRefresh()],
+  // uncomment the line with the base attribute to use the context path /admin/
+  // base: '/admin/',
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080',
     },
-    server: {
-        proxy: {
-            '/api': 'http://localhost:8080'
-        }
-    }
-})
+  },
+});
--- templates/front/vite.config.ts
+++ templates/front/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [reactRefresh()],
-    build: {
-        outDir: 'build'
+  plugins: [reactRefresh()],
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080',
     },
-    server: {
-        proxy: {
-            '/api': 'http://localhost:8080'
-        }
-    }
-})
+  },
+});
