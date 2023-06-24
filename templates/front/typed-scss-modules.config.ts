5adbc25504c9ffa23213b3984be62d72ff694896
Wail-LT <wlatif-ext@coreoz.com>
2023-06-24 10:33:17 +0200

    fix code review comments

--- templates/front/typed-scss-modules.config.ts
+++ templates/front/typed-scss-modules.config.ts
@@ -1,3 +1,4 @@
+// This is configuration for the `typed-scss-modules` package to enable SCSS modules
 export default {
   aliases: { "@scssVariables": "assets/scss/variables" },
   outputFolder: 'scss-types',
