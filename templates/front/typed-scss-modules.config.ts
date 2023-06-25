bd642ad6ba051baf20417440f0ab4ddaf767af7a
Wail-LT <wlatif-ext@coreoz.com>
2023-06-25 21:52:33 +0200

    fix scss type generation

--- templates/front/typed-scss-modules.config.ts
+++ templates/front/typed-scss-modules.config.ts
@@ -2,5 +2,7 @@
 export default {
   aliases: { '@scssVariables': 'assets/scss/variables' },
   outputFolder: 'scss-types',
+  nameFormat: ['dashes', 'none'],
   additionalData: '@use \'@scssVariables\' as *;',
+  exportType: 'default',
 };
