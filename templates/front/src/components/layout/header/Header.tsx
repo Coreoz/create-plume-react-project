f2a5507ef6d0e69c9155f5f8411af8ac0de609c7
Wail-LT <wlatif-ext@coreoz.com>
2023-07-06 17:31:48 +0200

    retour mr

--- templates/front/src/components/layout/header/Header.tsx
+++ templates/front/src/components/layout/header/Header.tsx
@@ -1,21 +1,21 @@
-import LocaleSelector from '@components/theme/LocaleSelector';
-import LocaleService from '@i18n/locale/LocaleService';
 import React from 'react';
 import { getGlobalInstance } from 'plume-ts-di';
 import { useObservable } from 'micro-observables';
+import { Locale } from '@lib/locale-resolver/LocaleResolver';
+import LocaleSelector from '@components/theme/LocaleSelector';
+import LocaleService from '@i18n/locale/LocaleService';
+
 import scss from './header.module.scss';
 
 function LocaleSelectorContainer() {
-  const localeService = getGlobalInstance(LocaleService);
-  const currentLocale = useObservable(localeService.getCurrentLocale());
+  const localeService: LocaleService = getGlobalInstance(LocaleService);
+  const currentLocale: Locale = useObservable(localeService.getCurrentLocale());
 
-  return (
-    <LocaleSelector
-      currentLocale={currentLocale}
-      availableLocales={localeService.getAvailableLocales()}
-      onLocaleSelected={(newLocale) => localeService.setCurrentLocale(newLocale)}
-    />
-  );
+  return <LocaleSelector
+    currentLocale={currentLocale}
+    availableLocales={localeService.getAvailableLocales()}
+    onLocaleSelected={(newLocale: Locale) => localeService.setCurrentLocale(newLocale)}
+  />;
 }
 
 export default function Header() {
