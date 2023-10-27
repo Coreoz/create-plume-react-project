7a7eb44f8006f6c7bb7a3b8c3e4c44851a010590
Aurélien Manteaux <amanteaux@coreoz.com>
2023-10-27 15:37:23 +0200

    Use browser-user-session

--- templates/admin/src/services/services-module.ts
+++ templates/admin/src/services/services-module.ts
@@ -1,17 +1,12 @@
 import { Scheduler } from 'simple-job-scheduler';
 import { Injector } from 'plume-ts-di';
+import { BrowserUserActivityListener, IdlenessDetector, UserActivityListener } from 'browser-user-session';
 import SessionService from './session/SessionService';
-import IdlenessDetector from '../lib/user-session/IdlenessDetector';
-import BrowserPageActivityManager from '../lib/user-session/page-activity/BrowserPageActivityManager';
-import PageActivityManager from '../lib/user-session/page-activity/PageActivityManager';
-import BrowserUserActivityListener from '../lib/user-session/user-activity/BrowserUserActivityListener';
 import ObservableNotificationEngine from '../lib/plume-notification/ObservableNotificationEngine';
-import UserActivityListener from '../lib/user-session/user-activity/UserActivityListener';
 import NotificationEngine from '../lib/plume-notification/NotificationEngine';
 
 export default function installServicesModule(injector: Injector) {
   // browser dependent services
-  injector.registerSingleton(BrowserPageActivityManager, PageActivityManager);
   injector.registerSingleton(BrowserUserActivityListener, UserActivityListener);
   // other services
   injector.registerSingleton(IdlenessDetector);
