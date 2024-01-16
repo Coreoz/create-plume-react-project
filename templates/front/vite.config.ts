3cdd99ff624a1c164788d1f1426efdcf37a5b6b3
Vincent Dubois <vdubois@coreoz.com>
2024-01-16 12:48:57 +0100

    Change css modules generation from dashed to camelCase

--- templates/front/vite.config.ts
+++ templates/front/vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
   },
   css: {
     modules: {
-      localsConvention: 'dashes',
+      localsConvention: 'camelCase',
     },
     preprocessorOptions: {
       scss: {
