b0440d97c8cdd50f74bba437037ed2e4173eb9cf
Vincent Dubois <vdubois@coreoz.com>
2024-04-05 15:05:50 +0200

    Change format of translation

--- templates/front/src/i18n/translations/en.ts
+++ templates/front/src/i18n/translations/en.ts
@@ -2,50 +2,67 @@ import { Translations } from './Translations';
 
 const enMessages: Translations = {
   // actions
-  'action.back': 'Back',
-  'action.cancel': 'Cancel',
-  'action.save': 'Save',
-  'action.delete': 'Delete',
-  'action.search': 'Search',
-  'action.add': 'Add',
-  'action.authenticate': 'Log in',
-  'action.disconnect': 'Log out',
-  'action.keep-editing': 'Keep editing',
-  'action.close-without-saving': 'Close without saving',
+  action: {
+    back: 'Back',
+    cancel: 'Cancel',
+    save: 'Save',
+    delete: 'Delete',
+    search: 'Search',
+    add: 'Add',
+    authenticate: 'Log in',
+    disconnect: 'Log out',
+    keep_editing: 'Keep editing',
+    close_without_saving: 'Close without saving',
+  },
   // common labels
-  'label.creation-date': 'Creation date',
-  'label.loading': 'Loading...',
+  label: {
+    creation_date: 'Creation date',
+    loading: 'Loading...',
+  },
   // common messages
-  'message.changes-saved': 'Changes have been successfully saved',
-  'message.unsaved-data': 'There are unsaved changes. '
-    + 'If you would like to save changes, press the "Keep editing" button',
-  'message.confirm-delete': 'To confirm the deleting, press the "Delete" button',
+  message: {
+    changes_saved: 'Changes have been successfully saved',
+    unsaved_data: 'There are unsaved changes. '
+      + 'If you would like to save changes, press the "Keep editing" button',
+    confirm_delete: 'To confirm the deleting, press the "Delete" button',
+  },
   // navigation
-  'app.name': 'Plume admin',
-  'nav.home': 'Home',
-  'nav.users': 'User management',
-  'nav.user-list': 'Users',
+  nav: {
+    home: 'Home',
+    users: 'User management',
+    user_list: 'Users',
+  },
   // home
-  'home.title': 'Home page',
-  'login.title': 'Please authenticate',
+  home: {
+    title: 'Home page',
+  },
+  login: {
+    title: 'Please authenticate',
+  },
   // users
-  'users.USERNAME': 'User name',
-  'users.PASSWORD': 'Password',
-  'users.PASSWORD_CONFIRM': 'Password confirmation',
-  'users.EMAIL': 'Email',
-  'users.FIRSTNAME': 'First name',
-  'users.LASTNAME': 'Last name',
-  'users.ROLE': 'Role',
+  users: {
+    userName: 'User name',
+    password: 'Password',
+    email: 'Email',
+    firstName: 'First name',
+    lastName: 'Last name',
+    role: 'Role',
+  },
   // pages users
-  'user.title-list': 'Users list',
-  'user.title-create': 'User creation',
-  'user.title-edit': 'User modification',
-  'user.error-passwords-different': 'Password do not match its confirmation',
-  // sample with pluralization
-  'clicks.count': (count: number) => `There is ${count} click${count > 1 ? 's' : ''} !`,
+  user: {
+    title_list: 'Users list',
+    title_create: 'User creation',
+    title_edit: 'User modification',
+    password_confirm: 'Password confirmation',
+    error_passwords_different: 'Password do not match its confirmation',
+  },
   // errors
-  'error.field.required': 'Field is required',
-  'error.field.email-wrong-format': 'The input email address is invalid',
+  error: {
+    field: {
+      required: 'Field is required',
+      email_wrong_format: 'The input email address is invalid',
+    },
+  },
   'http-errors': {
     INTERNAL_ERROR: 'An unexpected error occurred',
     NETWORK_ERROR: 'Network error, your internet connexion seems unavailable',
@@ -53,7 +70,7 @@ const enMessages: Translations = {
     FORBIDDEN_ERROR: 'It seems you do not have access to this resource or this action',
     WRONG_LOGIN_OR_PASSWORD: 'User name or password incorrect',
     // eslint-disable-next-line max-len
-    TOO_MANY_WRONG_ATTEMPS: (seconds: string) => `Due to login attempt errors, your account is locked for ${seconds} seconds, please try again later`,
+    TOO_MANY_WRONG_ATTEMPTS: (seconds: string) => `Due to login attempt errors, your account is locked for ${seconds} seconds, please try again later`,
     FIELD_REQUIRED: (fieldName: string) => `Field '${fieldName}' is required`,
     MESSAGE: (message: string) => message,
   },
