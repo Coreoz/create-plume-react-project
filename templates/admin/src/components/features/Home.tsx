4ff1810c42737fefcf8f8bbd2045272a9510d84d
Lucas Amiaud <lamiaud@coreoz.com>
2024-04-22 15:46:29 +0200

    plume theme destruct

--- templates/admin/src/components/features/Home.tsx
+++ templates/admin/src/components/features/Home.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import PlumeAdminTheme from '../../lib/plume-admin-theme/PlumeAdminTheme';
-import usePlumeTheme from '../hooks/ThemeHook';
+import usePlumeTheme, { PlumeAdminThemeComponents } from '../hooks/ThemeHook';
 import useMessages, { Messages } from '../../i18n/hooks/messagesHook';
 
 export default function Home() {
   const { messages }: Messages = useMessages();
-  const { panel: Panel, panelTitle: PanelTitle }: PlumeAdminTheme = usePlumeTheme();
+  const { Panel, PanelTitle }: PlumeAdminThemeComponents = usePlumeTheme();
 
   return (
     <Panel>
--- templates/admin/src/components/hooks/ThemeHook.ts
+++ templates/admin/src/components/hooks/ThemeHook.ts
@@ -1,8 +1,23 @@
 import { getGlobalInstance } from 'plume-ts-di';
+import { useMemo } from 'react';
 import PlumeAdminTheme from '../../lib/plume-admin-theme/PlumeAdminTheme';
 
-function usePlumeTheme(): PlumeAdminTheme {
-  return getGlobalInstance(PlumeAdminTheme);
+export type PlumeAdminThemeComponents = {
+  [K in keyof PlumeAdminTheme as Capitalize<string & K>]: PlumeAdminTheme[K]
+};
+
+function usePlumeTheme(): PlumeAdminThemeComponents {
+  const theme: PlumeAdminTheme = getGlobalInstance(PlumeAdminTheme);
+
+  return useMemo(() => Object.entries(theme)
+    /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+    .reduce((acc: PlumeAdminThemeComponents, [key, value]: [string, any]) => {
+        const capitalizedKey: string = key.charAt(0).toUpperCase() + key.slice(1);
+        acc[capitalizedKey as keyof PlumeAdminThemeComponents] = value;
+        return acc;
+      },
+      {} as PlumeAdminThemeComponents,
+    ), []);
 }
 
 export default usePlumeTheme;
