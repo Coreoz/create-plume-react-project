206ed8d2333919e3536ce997e3e69ccb0877f9ce
Aurélien Manteaux <amanteaux@coreoz.com>
2024-06-20 12:51:12 +0200

    Use plugin-react-swc instead of plugin-react

--- templates/admin/vite.config.ts
+++ templates/admin/vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      // eslint-disable-next-line consistent-return
+      parserConfig(id: string) {
+        // fix hot reloading with js files exported by TypeScript in the ts-built directory
+        if (id.endsWith('.js')) return { syntax: 'ecmascript', jsx: true };
+      },
+    }),
+  ],
   // uncomment the line with the base attribute to use the context path /admin/
   // base: '/admin/',
   build: {
